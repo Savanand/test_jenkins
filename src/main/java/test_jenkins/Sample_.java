a2c01964fa5827d0fc1e3a9e3ccc74d87207bbcc
Aniket Savanand <aniketpsavanand@gmail.com>
2017-07-18 17:01:26 -0700

    added src file

---
+++ src/main/java/test_jenkins/Sample_.java
@@ -0,0 +1,10 @@
+package test_jenkins;
+
+public class Sample_ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("sample from main");
+	}
+
+}
